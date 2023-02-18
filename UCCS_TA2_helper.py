fix ball_location computation with zero blocks

UCCS_TA2_helper.py
@@ -1918,11 +1918,13 @@ class UCCSTA2():
 
     def ball_location_error(self, statevector):  # if step 10 or 45, get long term ball position error computed from initial state vs current
 #        if(not (self.uccscart.tick  ==10 or ((self.uccscart.tick%45) ==0))):
-        if(not (self.uccscart.tick  ==10 or (self.uccscart.tick == 45))):            
+        numblocks = int((len(statevector)-13)/6)
+        if((not (self.uccscart.tick  ==10 or (self.uccscart.tick == 45)))):            
             return 0
+        if(numblocks<2):
+            return 1
         err = 0
         zerr=0
-        numblocks = int((len(statevector)-13)/6)
         prob=0
         diffs = np.zeros(3)
         nb=0
@@ -1941,7 +1943,6 @@ class UCCSTA2():
             diffs = diffs/nb
         else:
             err = 0
-            diffs = 0
             
         zerr = diffs[2] #last error is just z
         ratio = zerr / err  #get zerror a fraction of total eror